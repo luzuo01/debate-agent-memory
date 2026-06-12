9d1509215f0cda6ead7f6ee03db54bb5a3f75c82
luzuo01 <zhangfutongfd@163.com>
2026-06-12 14:33:44 +0800

    Revert "Add layered context memory API"
    This reverts commit 440d641abbbcacd8cf58430649f8bf425d7df589.

--- src/debate_agent_memory/__init__.py
+++ src/debate_agent_memory/__init__.py
@@ -1,18 +1,5 @@
 """Decoupled memory utilities for debate-style agents."""
 
-from debate_agent_memory.context_memory import ContextMemory
-from debate_agent_memory.context_models import (
-    BudgetSnapshot,
-    ContextPayload,
-    CurrentFocus,
-    LedgerRecord,
-    LedgerStatus,
-    PressureLevel,
-    StaticBrief,
-    SummaryRecord,
-    TurnRecord,
-    UnfinishedState,
-)
 from debate_agent_memory.core import AgentMemory
 from debate_agent_memory.formatters import PromptMemoryFormatter
 from debate_agent_memory.models import MemoryFilter, MemoryRecord, MemorySearchResult
@@ -21,24 +8,13 @@ from debate_agent_memory.stores import InMemoryStore, JsonlMemoryStore, MemorySt
 
 __all__ = [
     "AgentMemory",
-    "BudgetSnapshot",
-    "ContextMemory",
-    "ContextPayload",
-    "CurrentFocus",
     "InMemoryStore",
     "JsonlMemoryStore",
-    "LedgerRecord",
-    "LedgerStatus",
     "LexicalMemoryRetriever",
     "MemoryFilter",
     "MemoryRecord",
     "MemorySearchResult",
     "MemoryStore",
-    "PressureLevel",
     "PromptMemoryFormatter",
     "SQLiteMemoryStore",
-    "StaticBrief",
-    "SummaryRecord",
-    "TurnRecord",
-    "UnfinishedState",
 ]
